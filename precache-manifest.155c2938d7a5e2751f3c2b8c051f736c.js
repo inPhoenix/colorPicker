60ca0bcb609d2c763be5ae7f644cd5d4029f6b16
inPhoenix <spiderk@gmail.com>
2020-02-16 19:25:13 +0000

    Updates

---
+++ precache-manifest.155c2938d7a5e2751f3c2b8c051f736c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c8e37d5df84443588ed133116235b4",
+    "url": "/colorPicker/index.html"
+  },
+  {
+    "revision": "50ec6fc77c0c41429265",
+    "url": "/colorPicker/static/css/main.44331b64.chunk.css"
+  },
+  {
+    "revision": "aa5fb449fda0cbbcbabc",
+    "url": "/colorPicker/static/js/2.f25a7616.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/colorPicker/static/js/2.f25a7616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ec6fc77c0c41429265",
+    "url": "/colorPicker/static/js/main.486d1754.chunk.js"
+  },
+  {
+    "revision": "6a53f4f855a74652d775",
+    "url": "/colorPicker/static/js/runtime-main.42166a38.js"
+  }
+]);
