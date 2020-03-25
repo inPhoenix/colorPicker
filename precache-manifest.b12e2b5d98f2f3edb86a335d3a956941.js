f59b2325df18f95fc95c1e071feb35ff84fac33b
inPhoenix <spiderk@gmail.com>
2020-03-25 20:53:40 -0300

    Updates

---
+++ precache-manifest.b12e2b5d98f2f3edb86a335d3a956941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aed067d09169cd2e7c2f2769a2a1d74",
+    "url": "/colorPicker/index.html"
+  },
+  {
+    "revision": "293359270947ee3867ac",
+    "url": "/colorPicker/static/css/main.8ed53a7a.chunk.css"
+  },
+  {
+    "revision": "96863a4c3feaac2b2618",
+    "url": "/colorPicker/static/js/2.74336935.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/colorPicker/static/js/2.74336935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293359270947ee3867ac",
+    "url": "/colorPicker/static/js/main.0f05de8d.chunk.js"
+  },
+  {
+    "revision": "6a53f4f855a74652d775",
+    "url": "/colorPicker/static/js/runtime-main.42166a38.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/colorPicker/precache-manifest.21ca391d27a90fdaf87d1f739bedc56f.js"
+  "/colorPicker/precache-manifest.b12e2b5d98f2f3edb86a335d3a956941.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/colorPicker/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
